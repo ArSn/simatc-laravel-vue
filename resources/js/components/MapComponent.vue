cb1913ac9f4153169423e07918671d63c4c36c13
kolja <koljaz@web.de>
2019-02-23 17:54:01 +0100

    Built data structure in map for area zooming.

--- resources/js/components/MapComponent.vue
+++ resources/js/components/MapComponent.vue
@@ -6,19 +6,48 @@
     import * as L from "leaflet";
 
     export default {
+        data() {
+            return {
+                position: {
+                    lat: 52.55835117305416,
+	                lng: 13.289337158203127,
+                    zoom: 14,
+                },
+	            map: null,
+            };
+        },
+
+	    methods: {
+		    flyTo() {
+                console.log('called!');
+                this.map.flyTo(this.latLng, this.position.zoom);
+		    }
+	    },
+
+	    computed: {
+            latLng() {
+                return new L.LatLng(this.position.lat, this.position.lng);
+            },
+	    },
+
+        watch: {
+            position: {
+                handler() { this.flyTo(); },
+                deep: true,
+            },
+        },
+
         mounted() {
             const map = new L.Map('map');
+            this.map = map;
 
             // create the tile layer with correct attribution
             const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
             const osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
             const osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, zoomSnap: 0.1, attribution: osmAttrib});
 
-            // start the map in South-East England
-            map.setView(new L.LatLng(52.55835117305416, 13.289337158203127), 14);
+            map.setView(this.latLng, this.position.zoom);
             map.addLayer(osm);
-
-            map.zoomIn(0.2);
 
             map.on('zoomend', function() {
                 console.log(map.getZoom());
